a1abdeea8a07b8abb5c2bcfc46e1bfc855246f0e
JOANNASHIN <joannashin3@gmail.com>
2023-06-02 14:03:20 +0900

    header

---
+++ src/components/layouts/header/index.tsx
@@ -0,0 +1,16 @@
+interface Props {
+
+}
+
+const Header = (props:Props) => {
+    return (
+        <header>
+            header
+            <a href="">
+                /
+            </a>
+        </header>
+    )
+}  
+
+export default Header;
---
+++ src/components/layouts/header/style.tsx
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+const HeaderStyle = css`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 2rem;
+    background: #000;
+`;
+
+export default HeaderStyle;
