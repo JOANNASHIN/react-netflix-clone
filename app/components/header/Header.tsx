0a9963b22380b10827aa40102c162dc73dca90db
joannashin <joannashin3@gmail.com>
2023-06-12 22:21:58 +0900

    header style

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,54 +1,77 @@
+'use client';
+
 import Link from 'next/link';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 interface IHeaderProps {}
 
-const color = 'white';
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+
+  .left {
+    display: flex;
+    align-items: center;
+  }
+
+  .logo {
+    padding: 1rem;
+
+    img {
+      width: 10rem;
+    }
+  }
+
+  .menu {
+    display: flex;
+    margin-left: 1rem;
+
+    &__box {
+      padding: 0.5rem;
+
+      a {
+        color: #fff;
+        font-size: 1rem;
+        text-decoration: none;
+      }
+    }
+  }
+`;
 
 const Header = (props: IHeaderProps) => {
   return (
-    <header>
-      <div
-        css={css`
-          padding: 32px;
-          background-color: hotpink;
-          font-size: 24px;
-          border-radius: 4px;
-          &:hover {
-            color: ${color};
-          }
-        `}
-      >
-        Hover to change color.
-      </div>
-      <h1>
-        <Link href="/">
-          <figure>
+    <HeaderWrapper>
+      <div className="left">
+        <h1 className="logo">
+          <Link href="/">
             <img src="/images/logo.png" alt="Nexflix" />
-          </figure>
-        </Link>
-      </h1>
+          </Link>
+        </h1>
 
-      <ul>
-        <li>
-          <Link href="/">홈</Link>
-        </li>
-        <li>
-          <Link href="/">시리즈</Link>
-        </li>
-        <li>
-          <Link href="/">영화</Link>
-        </li>
-        <li>
-          <Link href="/">NEW! 요즘 대세 콘텐츠</Link>
-        </li>
-        <li>
-          <Link href="/">내가 찜한 콘텐츠</Link>
-        </li>
-        <li>
-          <Link href="/">언어별로 찾아보기</Link>
-        </li>
-      </ul>
+        <ul className="menu">
+          <li className="menu__box">
+            <Link href="/">홈</Link>
+          </li>
+          <li className="menu__box">
+            <Link href="/">시리즈</Link>
+          </li>
+          <li className="menu__box">
+            <Link href="/">영화</Link>
+          </li>
+          <li className="menu__box">
+            <Link href="/">NEW! 요즘 대세 콘텐츠</Link>
+          </li>
+          <li className="menu__box">
+            <Link href="/">내가 찜한 콘텐츠</Link>
+          </li>
+          <li className="menu__box">
+            <Link href="/">언어별로 찾아보기</Link>
+          </li>
+        </ul>
+      </div>
 
       {/* 퀵메뉴 */}
       <nav>
@@ -56,7 +79,7 @@ const Header = (props: IHeaderProps) => {
         <button>알림</button>
         <button>프로필</button>
       </nav>
-    </header>
+    </HeaderWrapper>
   );
 };
 
