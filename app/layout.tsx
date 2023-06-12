646fcc465be7d42406dd88ec8aa6cdcc44db8be4
joannashin <joannashin3@gmail.com>
2023-06-12 22:21:09 +0900

    reset scss 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,21 @@
+'use client';
+
+import styled from '@emotion/styled';
 import Header from './components/header/Header';
+import './styles/global.scss';
+
+const Body = styled.body`
+  min-width: 1100px;
+  background: #000;
+`;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Body>
+        <Header />
+        {children}
+      </Body>
     </html>
   );
 }
