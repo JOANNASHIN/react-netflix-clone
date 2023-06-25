5425dfea3e429f7f42e2c3468e444e58efa7d89a
joannashin <joannashin3@gmail.com>
2023-06-25 22:54:24 +0900

    video item component 마크업

---
+++ app/components/videoItem/index.tsx
@@ -0,0 +1,40 @@
+function VideoItem() {
+  return (
+    <>
+      <figure>
+        <img src="" alt="" />
+      </figure>
+
+      <div>
+        <figure>
+          <span>
+            TOP
+            <br />
+            <em>10</em>
+          </span>
+          <img src="" alt="" />
+          <span>
+            <span>다음 에피소드</span>
+            <span>월요일</span>
+          </span>
+        </figure>
+
+        <nav>
+          <button>play</button>
+          <button>plus</button>
+          <button>like</button>
+        </nav>
+
+        <div>
+          <span>15</span>
+          <span>에피소드 8개</span>
+          <span>HD</span>
+
+          <p>어두운 • 긴장감 넘치는 • 스릴러</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default VideoItem;
