65464b0ae25d38e619e560d8cbd90378582639c8
joannashin <joannashin3@gmail.com>
2023-06-25 23:29:09 +0900

    video item component 작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+import VideoItem from './components/videoItem';
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <VideoItem />
+    </>
+  );
 }
