73b11bf2c529d3ed7c58195c5456ea33bf48b748
joannashin <joannashin3@gmail.com>
2023-07-21 00:00:30 +0900

    login 마크업 & 스타일

--- app/containers/login/Login.style.tsx
+++ app/containers/login/Login.style.tsx
@@ -1,9 +1,113 @@
+import { color } from '@/styles/variants';
 import { css } from '@emotion/react';
 
 const style = css`
-  display: inline-block;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100%;
+
+  .login {
+    &__form {
+      padding: 60px 68px 150px;
+      background: ${color.black};
+      border-radius: 4px;
+      box-sizing: border-box;
+      color: ${color.white};
+
+      legend {
+        margin-bottom: 28px;
+        color: #fff;
+        font-size: 32px;
+        font-weight: 500;
+      }
+
+      &__input {
+        display: block;
+        margin-bottom: 16px;
+
+        input {
+          width: 100%;
+          height: 50px;
+          padding: 0 20px 0;
+          background: ${color.bgGrey};
+          border: 0;
+          border-radius: 4px;
+          color: #fff;
+          line-height: 50px;
+          box-sizing: border-box;
+        }
+      }
+
+      &__submit {
+        width: 100%;
+        padding: 10px 0;
+        background: ${color.red};
+        border-radius: 4px;
+        border: 0;
+        color: ${color.white};
+        font-size: 16px;
+        font-weight: 500;
+      }
+    }
+
+    &__footer {
+      &__division {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 20px;
+      }
+
+      &__save-id {
+        input {
+          display: none;
+
+          &:checked + span {
+            &:before {
+              background: ${color.bgGrey};
+            }
+          }
+        }
+
+        span {
+          display: flex;
+          align-items: center;
+          color: ${color.textGrey};
+          font-size: 13px;
+
+          &:before {
+            content: '';
+            display: inline-block;
+            width: 16px;
+            height: 16px;
+            margin-right: 10px;
+            background: ${color.white};
+            border: 0;
+            border-radius: 2px;
+          }
+        }
+      }
+
+      &__help {
+        color: ${color.textGrey};
+        font-size: 13px;
+        text-decoration: none;
+      }
+
+      &__text {
+        color: ${color.textGrey};
+        font-size: 16px;
+        font-weight: 400;
+      }
+
+      &__join {
+        color: ${color.white};
+        text-decoration: none;
+      }
+    }
+  }
 `;
 
 export default style;
--- app/containers/login/LoginContainer.tsx
+++ app/containers/login/LoginContainer.tsx
@@ -1,32 +1,42 @@
+import Link from 'next/link';
 import style from './Login.style';
 
 const LoginContainer = () => {
   return (
-    <div css={style}>
-      <form>
+    <div css={style} className="login">
+      <form className="login__form">
         <legend>로그인</legend>
         <fieldset>
-          <label>
+          <label className="login__form__input">
             <input type="text" placeholder="이메일 주소 또는 전화번호" />
           </label>
-          <label>
+          <label className="login__form__input">
             <input type="password" placeholder="비밀번호" />
           </label>
 
-          <button type="submit">로그인</button>
-          <div>
-            <label>
-              <input type="checkbox" />
-              <span>로그인 정보 저장</span>
-            </label>
+          <button type="submit" className="login__form__submit">
+            로그인
+          </button>
 
-            <a href="#">도움이 필요하신가요?</a>
-          </div>
+          <div className="login__footer">
+            <div className="login__footer__division">
+              <label className="login__footer__save-id">
+                <input type="checkbox" />
+                <span>로그인 정보 저장</span>
+              </label>
 
-          <div>
-            <p>
-              Netflix 회원이 아닌가요? <a href="#">지금 가입하세요.</a>
-            </p>
+              <a href="https://www.netflix.com/kr/LoginHelp" className="login__footer__help">
+                도움이 필요하신가요?
+              </a>
+            </div>
+            <div className="login__footer__division">
+              <p className="login__footer__text">
+                Netflix 회원이 아닌가요?&nbsp;
+                <Link href="/join" className="login__footer__join">
+                  지금 가입하세요.
+                </Link>
+              </p>
+            </div>
           </div>
         </fieldset>
       </form>
