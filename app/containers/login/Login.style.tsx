379d8396f92361844acbf3352699c6ebea94cf93
joannashin <joannashin3@gmail.com>
2023-07-20 22:59:19 +0900

    로그인페이지 작업

---
+++ app/containers/login/Login.style.tsx
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+
+const style = css`
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+`;
+
+export default style;
--- app/containers/login/LoginContainer.tsx
+++ app/containers/login/LoginContainer.tsx
@@ -1,7 +1,35 @@
+import style from './Login.style';
+
 const LoginContainer = () => {
   return (
-    <div>
-      <h1>login</h1>
+    <div css={style}>
+      <form>
+        <legend>로그인</legend>
+        <fieldset>
+          <label>
+            <input type="text" placeholder="이메일 주소 또는 전화번호" />
+          </label>
+          <label>
+            <input type="password" placeholder="비밀번호" />
+          </label>
+
+          <button type="submit">로그인</button>
+          <div>
+            <label>
+              <input type="checkbox" />
+              <span>로그인 정보 저장</span>
+            </label>
+
+            <a href="#">도움이 필요하신가요?</a>
+          </div>
+
+          <div>
+            <p>
+              Netflix 회원이 아닌가요? <a href="#">지금 가입하세요.</a>
+            </p>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 };
