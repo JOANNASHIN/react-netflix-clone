fe981e8610f6321a67997a9da3377e0aebc372a9
joannashin <joannashin3@gmail.com>
2023-08-20 23:01:38 +0900

    preview layer event

--- app/components/videoItem/VideoItem.style.tsx
+++ app/components/videoItem/VideoItem.style.tsx
@@ -6,75 +6,13 @@ const color = {
   white: '#fff',
 };
 
-const videoItemStyles = css`
+const styles = css`
   .video-item {
     position: relative;
+    cursor: pointer;
     // test
     width: 300px;
   }
-
-  .preview-layer {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    background: ${color.black};
-    border-radius: 0.5rem;
-    color: ${color.white};
-
-    .layer {
-      &__badge {
-        &--top {
-          position: absolute;
-          top: 0;
-          right: 0;
-          padding: 0.3rem;
-          background: ${color.red};
-          color: white;
-          font-weight: bold;
-          font-size: 0.8rem;
-          text-align: center;
-
-          em {
-            font-size: 1.5rem;
-          }
-        }
-      }
-
-      &__img {
-        overflow: hidden;
-        position: relative;
-        width: 100%;
-        border-radius: 0.5rem;
-
-        img {
-          position: absolute;
-          top: 0;
-          left: 0;
-          max-width: 100%;
-          object-fit: cover;
-        }
-
-        &:after {
-          content: '';
-          display: block;
-          padding-bottom: 50%;
-        }
-      }
-
-      &__detail {
-        padding: 1.5rem 1rem;
-
-        &__type {
-          margin-top: 0.4rem;
-        }
-      }
-
-      &__quick {
-        margin-bottom: 0.5rem;
-      }
-    }
-  }
 `;
 
-export default videoItemStyles;
+export default styles;
--- app/components/videoItem/VideoItem.tsx
+++ app/components/videoItem/VideoItem.tsx
@@ -1,47 +1,28 @@
-import React from 'react';
-import videoItemStyles from './VideoItem.style';
+import React, { useState } from 'react';
+import styles from './VideoItem.style';
+import VideoPreview from '../VideoPreview/VideoPreview';
 
-function VideoItem() {
+const VideoItem = () => {
+  const [showPreview, setShowPreview] = useState(false);
+
+  const handleOpenPreview = () => {
+    setShowPreview(!showPreview);
+  };
+
   const testImage =
     'https://occ-0-2218-988.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfGwCHkfVt0VvppMbdJP500EImDcpdIH1huv3tO9oVlh4JtEIMPjM1E5XkNGruJjWnW_WOKRLD6DkDBYCX_-rjWsu3f-nvWYOsJltHrxYhwuypa2qeE681p1ZXo4L3wXcPAurq7ZFmgi9E5OrHBPoVGqSAdOihVvfOh0YLvrpPDHTWG5jnDx2rXzGIenI63UERcCfjbzheHpoO4sNfLC-eP4V5DQCVcI2nhhbAlf9WU_86ujSdhL1g_Z04kpRcFpPYFYC8ZjAnJ9iFuT5yM29lQyMwzQsfYRpXSQqVV-4yfnkqFWzaC5fXl-qVYEdPM1L15jHhMFjfuoiWPdNIMWG8D-UUnbrcXk17eIL2Wy1tUKIEOQ2te70g.jpg?r=808';
+
   return (
-    <div css={videoItemStyles}>
+    <div css={styles}>
       <div className="video-item">
-        <figure className="video-item__img">
+        <figure className="video-item__img" onClick={handleOpenPreview}>
           <img src={testImage} alt="" />
         </figure>
 
-        <div className="preview-layer">
-          <div className="layer__img">
-            <span className="layer__badge--top">
-              TOP
-              <br />
-              <em>10</em>
-            </span>
-            <figure>
-              <img src={testImage} />
-            </figure>
-          </div>
-
-          <div className="layer__detail">
-            <nav className="layer__quick">
-              <button>play</button>
-              <button>plus</button>
-              <button>like</button>
-            </nav>
-
-            <p>
-              <span>15</span>
-              <span>에피소드 8개</span>
-              <span>HD</span>
-            </p>
-
-            <p className="layer__detail__type">어두운 • 긴장감 넘치는 • 스릴러</p>
-          </div>
-        </div>
+        {showPreview && <VideoPreview />}
       </div>
     </div>
   );
-}
+};
 
 export default VideoItem;
